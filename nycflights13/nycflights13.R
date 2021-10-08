ffe89ace3a49ac0a8889d3802473634629ac6feb
Troels Laegsgaard <troels.laegsgaard@gmail.com>
2021-10-08 17:42:59 +0200

    NA as empty space.

--- nycflights13/nycflights13.R
+++ nycflights13/nycflights13.R
@@ -1,7 +1,7 @@
 library(nycflights13)
 
-write.csv(airlines, 'nycflights13/airlines.csv', row.names=FALSE)
-write.csv(airports, 'nycflights13/airports.csv', row.names=FALSE)
-write.csv(flights, 'nycflights13/flights.csv', row.names=FALSE)
-write.csv(planes, 'nycflights13/planes.csv', row.names=FALSE)
-write.csv(weather, 'nycflights13/weather.csv', row.names=FALSE)
+write.csv(airlines, 'nycflights13/airlines.csv', na='', row.names=FALSE)
+write.csv(airports, 'nycflights13/airports.csv', na='', row.names=FALSE)
+write.csv(flights, 'nycflights13/flights.csv', na='', row.names=FALSE)
+write.csv(planes, 'nycflights13/planes.csv', na='', row.names=FALSE)
+write.csv(weather, 'nycflights13/weather.csv', na='', row.names=FALSE)
